204ade8439ec222650821d7b3de80138d3f4721c
水墨青花 <1550518923@qq.com>
2020-03-04 19:34:30 +0800

    springboot 静态资源文件的自定义配置、以及注解的使用说明

--- boot-helloworld/src/main/java/com/wxw/BootHelloworldApplication.java
+++ boot-helloworld/src/main/java/com/wxw/BootHelloworldApplication.java
@@ -1,8 +1,11 @@
-package com.wxw.boothelloworld;
+package com.wxw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication  标注一个主程序类，说明是一个springboot程序
+ */
 @SpringBootApplication
 public class BootHelloworldApplication {
 
---
+++ boot-helloworld/src/main/java/com/wxw/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.wxw.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: wxw
+ * @create: 2020-03-04-14:24
+ */
+@Controller
+public class  HelloController{
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
