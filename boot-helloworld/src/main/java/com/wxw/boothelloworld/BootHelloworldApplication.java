1f24fc1d1544b9bed89dc1c861a0bd6b765236c1
水墨青花 <1550518923@qq.com>
2020-03-04 12:34:10 +0800

    刚创建boot

---
+++ boot-helloworld/src/main/java/com/wxw/boothelloworld/BootHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.wxw.boothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootHelloworldApplication.class, args);
+    }
+
+}
---
+++ boot-helloworld/src/test/java/com/wxw/boothelloworld/BootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxw.boothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
