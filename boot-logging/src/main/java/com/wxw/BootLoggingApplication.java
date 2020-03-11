51d43c3270e4dcea711f4da8ff9c25f02b9507ad
水墨青花 <1550518923@qq.com>
2020-03-12 00:58:25 +0800

    添加模板引擎

---
+++ boot-logging/src/main/java/com/wxw/BootLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLoggingApplication.class, args);
+    }
+
+}
---
+++ boot-web/src/test/java/com/wxw/BootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
