58f13606e72fe50732b0867baa920572b92d5245
水墨青花 <1550518923@qq.com>
2020-03-14 00:21:05 +0800

    使用模板引擎

--- boot-web/src/main/java/com/wxw/controller/HelloController.java
+++ boot-web/src/main/java/com/wxw/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.wxw.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @Author: wxw
  * @create: 2020-03-09-21:59
  */
+@Controller
 @RestController
 public class HelloController {
 
@@ -15,5 +19,13 @@ public class HelloController {
     @RequestMapping("/hello")
     public String helloTest(){
         return "Hello World!";
+    }
+
+    // 查出一些数据在页面展示
+    @RequestMapping("/success")
+    public String SuccessTest(Map<String,Object> map){
+        map.put("hello", "你好");
+        //classpath:/templates/ 下的.html文件
+        return "success";
     }
 }
