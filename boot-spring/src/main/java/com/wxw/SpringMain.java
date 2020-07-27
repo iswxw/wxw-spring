fe5997a72a47b374e1c8bce88d422304d22d8069
魏小伟 <1550518923@qq.com>
2020-07-27 08:34:48 +0800

    新增spring解析模块

---
+++ boot-spring/src/main/java/com/wxw/SpringMain.java
@@ -0,0 +1,17 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ Author ：wxw.
+ * @ Date ： 8:25 2020/7/27
+ * @ Description：Spring 启动类
+ * @ Version:   v_0.0.1
+ */
+@SpringBootApplication
+public class SpringMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMain.class, args);
+    }
+}
