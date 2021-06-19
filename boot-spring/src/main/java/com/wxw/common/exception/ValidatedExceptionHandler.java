84c15ac6d8867ab36dc904a987ed07d25f3f7c97
weixiaowei <weixiaowei@baijia.com>
2021-06-19 13:03:06 +0800

    Spring参数校验

--- boot-spring/src/main/java/com/wxw/common/exception/ValidatedExceptionHandler.java
+++ boot-spring/src/main/java/com/wxw/common/exception/ValidatedExceptionHandler.java
@@ -4,6 +4,7 @@ import com.wxw.common.response.Result;
 import com.wxw.common.helper.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -41,5 +44,26 @@ public class ValidatedExceptionHandler {
             }
         }
         return ResultUtil.validatedException(stringBuilder.toString());
+    }
+
+
+    /**
+     * 统一处理参数校验错误异常(非Spring接口数据绑定验证)
+     *
+     * @param response
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public Result<?> processValidException(HttpServletResponse response, BindException e) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        //获取校验错误结果信息，并将信息组装
+        List<String> errorStringList = e.getBindingResult().getAllErrors()
+                .stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
+        String errorMessage = String.join("; ", errorStringList);
+        response.setContentType("application/json;charset=UTF-8");
+        log.error(e.toString() + "_" + e.getMessage(), e);
+        return ResultUtil.validatedException(errorMessage);
     }
 }
